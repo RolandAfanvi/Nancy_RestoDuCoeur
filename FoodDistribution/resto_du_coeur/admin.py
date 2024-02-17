58826abe5395e8e8e03347e42c31fed77c6e2c50
RolandAfanvi <125564906+RolandAfanvi@users.noreply.github.com>
2024-02-17 23:52:12 +0100

    Create the Model of the database

--- FoodDistribution/resto_du_coeur/admin.py
+++ FoodDistribution/resto_du_coeur/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Product,Order,OrderItem,DistributionSite,Category,Stock
 
 # Register your models here.
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ('name', 'date_added')
+
+
+class AdminOrder(admin.ModelAdmin):
+    list_display = ['date_order', 'distribution_site', 'get_products']
+
+    def get_products(self, obj):
+        return ", ".join([str(product) for product in obj.products.all()])
+
+    get_products.short_description = 'Products'
+
+    
+class AdminOrderItem(admin.ModelAdmin):
+    list_display = ('order', 'product','quantity')
+
+class AdminStock(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'last_updated')
+
+class AdminDistributionSite(admin.ModelAdmin):
+    list_display = ('name', 'address', 'commune')
+
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ('name', 'category', 'image', 'stock', 'date_added')
+
+
+admin.site.register(Product,AdminProduct)
+admin.site.register(Order,AdminOrder)
+admin.site.register(Category,AdminCategory)
+admin.site.register(OrderItem,AdminOrderItem)
+admin.site.register(Stock,AdminStock)
+admin.site.register(DistributionSite,AdminDistributionSite)
--- FoodDistribution/resto_du_coeur/models.py
+++ FoodDistribution/resto_du_coeur/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date_added']
+    def __str__(self):
+        return self.name    
+
+
+class DistributionSite(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    commune = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name  
+      
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = ForeignKey(Category, related_name='categorie', on_delete=models.CASCADE)
+    image = models.CharField(max_length=5000)
+    date_added = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date_added']  
+
+    def __str__(self):
+        return self.name     
+
+class Stock(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.quantity}"
+
+
+    
+
+class Order(models.Model):
+    date_order = models.DateTimeField(auto_now_add=True)
+    distribution_site = models.ForeignKey(DistributionSite, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='OrderItem')
+
+    class Meta:
+        ordering = ['-date_order']
+
+
+    def __str__(self):
+        return self.distribution_site.name  
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+
